dd111118022ca56a922883e21ccfa52b07026503
Gamdel84 <gustavo.a.mtec@gmail.com>
2025-07-31 19:21:03 -0300

    fix: adapt for vercel4

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
 import express from "express";
+import path from "path";
 import { join, __dirname } from "./utils/index.js";
 import guitarRoutes from "./routes/guitar.route.js";
-import authRouter from './routes/auth.routes.js';
-import { envs } from './config/envs.js';
+import authRouter from "./routes/auth.routes.js";
+import { envs } from "./config/envs.js";
 
 const app = express();
 
-// middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// servir archivos estáticos
+// Servir archivos estáticos
 app.use(express.static(join(__dirname, "public")));
 
-// rutas
+// Rutas
 app.use("/api/guitars", guitarRoutes);
 app.use("/auth", authRouter);
 
-// ruta raíz que devuelve el frontend
+// Ruta raíz
 app.get("/", (req, res) => {
-  res.sendFile(join(__dirname, "public", "index.html"));
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
 });
 
-// ⚠️ No usar app.listen() en Vercel
 export default app;
+
 
 
 
