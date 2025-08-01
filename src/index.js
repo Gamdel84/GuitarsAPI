972c7bbdac11b2f3a3402cb526da6c144e070f49
Gamdel84 <gustavo.a.mtec@gmail.com>
2025-07-31 21:48:08 -0300

    Quitando la carpeta public de src a la raiz y cambio en archivo src/index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,24 @@
-import express from "express";
-import path from "path";
 import { join, __dirname } from "./utils/index.js";
 import guitarRoutes from "./routes/guitar.route.js";
 import authRouter from "./routes/auth.routes.js";
-import { envs } from "./config/envs.js";
+import express from "express";
+import path from "path";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// Servir archivos estáticos
 app.use(express.static(join(__dirname, "public")));
-
-// Rutas
 app.use("/api/guitars", guitarRoutes);
 app.use("/auth", authRouter);
 
-// Ruta raíz
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "public", "index.html"));
 });
 
-export default app;
+export default function handler(req, res) {
+  app(req, res);
+}
 
 
 
